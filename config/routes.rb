c0aae32fb3a64e259895298370420e9dfe1db4ff
Eric Emminger <EricEmminger@gmail.com>
2014-03-21 08:01:40 -0400

    Generate appointments scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Appointments::Application.routes.draw do
+  resources :appointments
+
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
