e4d21eceaa0b833590cd16366eed043616411fd3
Eric Emminger <EricEmminger@gmail.com>
2014-03-24 17:46:47 -0400

    Import comma-delimited data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+csv_text = File.read('db/appt_data.csv')
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|
+  format = '%m/%d/%y %k:%M'
+  hash = row.to_hash
+  hash[:start_time] = DateTime.strptime(row[0], format)
+  hash[:end_time] = DateTime.strptime(row[1], format)
+  a = Appointment.new(hash)
+  a.imported = true
+  a.save
+end
